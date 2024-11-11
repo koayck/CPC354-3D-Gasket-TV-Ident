0dd9062a264deaad38a488eb0884874bedd77e01
Jisi-A <7216lyc@gmail.com>
2024-11-11 21:59:22 +0800

    fix: vec3 function if else condition

--- MV.js
+++ MV.js
@@ -134,13 +134,13 @@ function vec4() {
           out[2] = arguments[0][2];
           out[3] = arguments[0][3];
           return out;
+        } else if (arguments[0].type === "vec3") {
+          out[0] = arguments[0][0];
+          out[1] = arguments[0][1];
+          out[2] = arguments[0][2];
+          out[3] = 1.0;
+          return out;
         }
-      } else if (arguments[0].type == "vec3") {
-        out[0] = arguments[0][0];
-        out[1] = arguments[0][1];
-        out[2] = arguments[0][2];
-        out[3] = 1.0;
-        return out;
       } else {
         out[0] = arguments[0][0];
         out[1] = arguments[0][1];
